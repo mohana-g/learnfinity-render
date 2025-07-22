9777fa60bd6fefdcb8cdb246e9cf015142ff7a8b
mohana-g <141321264+mohana-g@users.noreply.github.com>
2025-07-22 15:10:06 +0530

    api.js

--- server/client/src/utils/api.js
+++ server/client/src/utils/api.js
@@ -7,7 +7,7 @@ const API = axios.create({
 // Add a function to test the API connection
 export const fetchTestMessage = async () => {
   try {
-    const response = await API.get('/test');
+    const response = await API.get('/api/test');
     return response.data;
   } catch (error) {
     console.error('Error fetching test message:', error);
