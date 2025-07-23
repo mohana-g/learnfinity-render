e236d6d87a0644ffd4504c1818e8ab475b1056af
mohana-g <141321264+mohana-g@users.noreply.github.com>
2025-07-23 09:51:30 +0530

    TrainerCourses.js

--- server/client/src/pages/TrainerDashboard/TrainerCourses/TrainerCourses.js
+++ server/client/src/pages/TrainerDashboard/TrainerCourses/TrainerCourses.js
@@ -9,7 +9,7 @@ const CoursesPage = () => {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/courses"); // Backend API URL
+        const response = await fetch("https://hilms.onrender.com/api/courses"); // Backend API URL
         if (!response.ok) {
           throw new Error("Failed to fetch courses");
         }
