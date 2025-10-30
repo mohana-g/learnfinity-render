2963fd64ad172e346f271a2b9bd0a8aabb228130
Mohana G <mohanamonag5@gmail.com>
2025-10-30 16:52:08 +0530

    navbar and footer style changed

--- server/client/src/styles/components/Footer.js
+++ server/client/src/styles/components/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <p>&copy; 2025 HILMS. All Rights Reserved.</p>
-        <p>Contact us: <a href="mailto:support@learnfinity.com">support@learnfinity.com</a> | <a href="tel:+919999999999">+91 9999999999</a></p>
+        <p>&copy; { new Date().getFullYear() } HILMS | HIROTEC INDIA PRIVATE LIMITED. All Rights Reserved.</p>
+        <p>Contact us: <a href="mailto:learn@hirotecindia.com">learn@hirotecindia.com</a> | <a href="tel:+919999999999">+91 9999999999</a></p>
         <ul className="footer-links">
           <li><a href="/terms">Terms of Service</a></li>
           <li><a href="/privacy">Privacy Policy</a></li>
