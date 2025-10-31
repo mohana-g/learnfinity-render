b76d4d1f5c11c95258984cef9116b0fcb51fdcfc
Mohana G <mohanamonag5@gmail.com>
2025-10-31 12:57:49 +0530

    uploadMiddleware path chaned to use path.join for uploads directory cb(null, path.join(__dirname, "..", "uploads"));

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -36,7 +36,8 @@ const path = require("path");
 // Storage configuration
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Store files in 'uploads/' folder
+    // cb(null, "uploads/"); // Store files in 'uploads/' folder
+    cb(null, path.join(__dirname, "..", "uploads")); // Store files in 'uploads/' folder
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
